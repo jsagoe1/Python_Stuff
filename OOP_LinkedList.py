8dcbc879db2478839be919823591b105a9a9a2a4
philippeitis <33013301+philippeitis@users.noreply.github.com>
2019-12-16 14:45:34 -0800

    Update OOP_LinkedList.py
    Fix whitespace, 
    Use index errors instead.
    Add get_node_at_index helper function.

--- OOP_LinkedList.py
+++ OOP_LinkedList.py
@@ -13,47 +13,47 @@ class LinkedList:
     def append(self, value):
         # Adds new node containing 'data' to the end of the linked list.
         new_node = Node(value)
-		self.tail.next = new_node
+        self.tail.next = new_node
         self.tail = new_node
         self.length += 1
-	
+
+    def get_node_at_index(self, index):
+	if index < 0 or index >= self.length:
+            raise IndexError("ERROR: index less than 0 or greater than length")
+
+        prev_node = self.head.next
+	for _ in range(index):
+            prev_node = prev_node.next
+	return prev_node
+
     def insert(self, index, value):
-		# Inserts a new node at index 'index' containing data 'data'.
-		# Indices begin at 0. If the provided index is greater than or 
-		# equal to the length of the linked list the 'data' will be appended.
-        if index < 0:
-            print("ERROR: index less than 0")
-            return
+        # Inserts a new node at index 'index' containing data 'data'.
+        # Indices begin at 0. If the provided index is greater than or 
+        # equal to the length of the linked list the 'data' will be appended.
         if index >= self.length:
             self.append(value)
-        idx = 0
-        prev_node = self.head
-        cur_node = self.head.next
-		
-	    for _ in range(index-1):
-		    prev_node = prev_node.next
-            cur_node = cur_node.next
-			
-	    new_node = Node(value)
-	    prev_node.next = new_node
-	    new_node.next = cur_node
+	
+        prev_node = get_node_at_index(index)
+        new_node = Node(value)
+	new_node.next = prev_node.next
+        prev_node.next = new_node
         self.length += 1
 		
     def erase(self, index):
         # Deletes the node at index 'index'.
-        if index >= self.length or index < 0: # added 'index<0' post-video
-            print("ERROR: 'Erase' Index out of range!")
+        if index >= self.length or index < 0: # added 'index < 0' post-video
+            raise IndexError("'Erase' Index (%d) out of range for a list of size %d!" % (index, self.length))
+	if index == 0:
+	    prev_node = self.head
+	    cur_node = prev_node.next
+            prev_node.next = cur_node.next
+            self.length -= 1
             return
-        cur_node = self.head.next
-        prev_node = self.head
-        
-		for _ in range(index-1):
-			prev_node = prev_node.next
-            cur_node = cur_node.next
 
-		prev_node.next = cur_node.next
-		self.length -= 1
-		return
+        prev_node = get_node_at_index(index - 1)
+	cur_node = prev_node.next
+	prev_node.next = cur_node.next
+        self.length -= 1
 
     def display(self):
         # Returns the linked list in traditional Python list format. 
@@ -72,38 +72,19 @@ class LinkedList:
         while cur_node.next != None:
             l_len += 1
             cur_node = cur_node.next
-	    assert l_len == self.length
+        assert l_len == self.length
         return l_len
     
     def get(self, index):
         # Returns the value of the node at 'index'. 
-        if index < 0 or index >= self.length:
-            print("ERROR: index out of range")
-            return
-        idx = 0
-        cur_node = self.head.next
-        while cur_node != None:
-            if idx == index:
-                return cur_node.value
-            cur_node = cur_node.next
-            idx += 1                         
+        return get_node_at_index(index).value
         
     def set(self, index, value):
         # Sets the data at index 'index' equal to 'data'.
-	    # Indices begin at 0. If the 'index' is greater than or equal 
-	    # to the length of the linked list a warning will be printed 
-	    # to the user.
-        if index >= self.length or index <0:
-            print("EEROR: 'Set' Index out of range!")
-            return
-        idx = 0
-        cur_node = self.head.next
-        while cur_node is not None:
-            if idx == index:
-                cur_node.value = value
-                return
-            cur_node = cur_node.next
-            idx += 1        
+        # Indices begin at 0. If the 'index' is greater than or equal 
+        # to the length of the linked list a warning will be printed 
+        # to the user.
+	get_node_at_index(index).value = value
         
     	
 ls = LinkedList()
