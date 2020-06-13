93cae7429eb836bc6433d1d58dc4b935b3c1cff5
Jojoe Sagoe <jojoe.sagoe@yahoo.com>
2020-06-12 17:15:14 -0700

    Update dijkstraShortestPath.py

--- dijkstraShortestPath.py
+++ dijkstraShortestPath.py
@@ -1,5 +1,6 @@
+
 # =============================================================================
-# We will create a dictionary to represent the graph
+# Graph represented by adjacency dictionary
 # =============================================================================
 graph = {
 
@@ -14,91 +15,52 @@ graph = {
 }
 
 
-def dijkstra(graph,start,goal):
-
-    shortest_distance = {} #dictionary to record the cost to reach to node. We will constantly update this dictionary as we move along the graph.
-    track_predecessor = {} #dictionary to keep track of path that led to that node.
-    unseenNodes = graph #to iterate through all nodes
-    infinity = 5000 #infinity can be considered a very large number
-    track_path = [] #dictionary to record as we trace back our journey
-
-
-
-# =============================================================================
-# Initially we want to assign 0 as the cost to reach to source node and infinity as cost to all other nodes
-# =============================================================================
-
-    for node in unseenNodes:
-        shortest_distance[node] = infinity
-
-    shortest_distance[start] = 0
-
-# =============================================================================
-# The loop will keep running until we have entirely exhausted the graph, until we have seen all the nodes
-# =============================================================================
-# =============================================================================
-# To iterate through the graph, we need to determine the min_distance_node every time.
-# =============================================================================
-
-    while unseenNodes:
-        min_distance_node = None
-
-        for node in unseenNodes:
-            if min_distance_node is None:
-                min_distance_node = node
-
-            elif shortest_distance[node] < shortest_distance[min_distance_node]:
-                min_distance_node = node
-
-# =============================================================================
-# From the minimum node, what are our possible paths
-# =============================================================================
-
-        path_options = graph[min_distance_node].items()
-
-
-# =============================================================================
-# We have to calculate the cost each time for each path we take and only update it if it is lower than the existing cost
-# =============================================================================
-
-        for child_node, weight in path_options:
-
-            if weight + shortest_distance[min_distance_node] < shortest_distance[child_node]:
-
-                shortest_distance[child_node] = weight + shortest_distance[min_distance_node]
-
-                track_predecessor[child_node] = min_distance_node
-
-# =============================================================================
-# We want to pop out the nodes that we have just visited so that we dont iterate over them again.
-# =============================================================================
-        unseenNodes.pop(min_distance_node)
-
-
-
-# =============================================================================
-# Once we have reached the destination node, we want trace back our path and calculate the total accumulated cost.
-# =============================================================================
-
-    currentNode = goal
-
-    while currentNode != start:
-
-        try:
-            track_path.insert(0,currentNode)
-            currentNode = track_predecessor[currentNode]
-        except KeyError:
-            print('Path not reachable')
-            break
-    track_path.insert(0,start)
-
-
-# =============================================================================
-#  If the cost is infinity, the node had not been reached.
-# =============================================================================
-    if shortest_distance[goal] != infinity:
-        print('Shortest distance is ' + str(shortest_distance[goal]))
-        print('And the path is ' + str(track_path))
-
-
-dijkstra(graph, 'a', 'h')
+def dijkstra(graph, start, goal):
+    infinity = 99999999999
+    shortestDistanceFromStart = {}  # to hold shortest distance to each vertex
+    prevVertex = {}                 # to keep track of vertices leading to curVertex
+    
+    #set all vertices to infinity initially to start with
+    for vertex in graph:
+        shortestDistanceFromStart[vertex] = infinity
+    #except start vertex, set to 0
+    shortestDistanceFromStart[start] = 0
+    
+    unvisitedVertices = []            #to hold unvisited vertices as we go through graph
+    visited = []                      #to hold visited vertices as we go through grapgh
+    
+    prevVertex[start] = None          # start has no prev node
+    unvisitedVertices.append(start)
+    
+    #repeat until unvisited is empty
+    while unvisitedVertices != []:
+        #pop last element in Unvisited
+        curVertex = unvisitedVertices.pop()
+        #get connected vertices of curVertex
+        for buddy, weight in graph[curVertex].items():
+            #calculate distance from cur_vertex to buddy
+            distance  = shortestDistanceFromStart[curVertex] + graph[curVertex][buddy]
+            #update shortestdistance val if distance is less than stored
+            if distance < shortestDistanceFromStart[buddy]:
+                prevVertex[buddy] = curVertex
+                shortestDistanceFromStart[buddy] = distance
+            #add buddies to unvisited 
+            if buddy not in unvisitedVertices and curVertex not in visited:
+                unvisitedVertices.insert(0, buddy)
+        #add curVertex to visited
+        visited.append(curVertex)
+        
+    path = []               # to trace path
+    cur = goal              # start from goal and trace back
+    while cur!=None:
+        path.insert(0,cur)
+        cur = prevVertex[cur]
+    if path[0] == start:
+        return "-->".join(path)
+    return []
+    
+            
+                
+            
+    
+print(dijkstra(graph, 'b', 'h'))
