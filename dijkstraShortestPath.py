faaa34a3f2893dcf7fc440076bc7066911ab3235
Jojoe Sagoe <jojoe.sagoe@yahoo.com>
2020-06-13 17:15:02 -0700

    Update dijkstraShortestPath.py

--- dijkstraShortestPath.py
+++ dijkstraShortestPath.py
@@ -1,4 +1,30 @@
+graph = {
+
+'a':{'b':3,'c':4, 'd':7},
+'b':{'c':1,'f':5},
+'c':{'f':6,'d':2},
+'d':{'e':3, 'g':6},
+'e':{'g':3, 'h':4},
+'f':{'e':1, 'h':8},
+'g':{'h':2},
+'h':{'g':2}
+}
+
+
+
 def dijkstra_reddit(graph, start, goal):
+    def getPath(start, goal, prevVertex):
+        path = []                       # to trace path
+        cur = goal                      # start from goal and trace back
+        while cur!=None:
+            if cur not in prevVertex:   #no path from start to goal
+                print("No path from start to goal")
+                return ([], 0)
+            path.insert(0,cur)
+            cur = prevVertex[cur]
+        return path   
+        
+    
     # check if start is same as goal
     if start in graph and start == goal:
         return ([start], 0)
@@ -43,13 +69,10 @@ def dijkstra_reddit(graph, start, goal):
                 unvisitedVertices[neighbour] = shortestDistanceFromStart[neighbour]
         #add curVertex to visited
         visited.append(curVertex)
-        
-    path = []                       # to trace path
-    cur = goal                      # start from goal and trace back
-    while cur!=None:
-        if cur not in prevVertex:   #no path from start to goal
-            print("No path from start to goal")
-            return ([], 0)
-        path.insert(0,cur)
-        cur = prevVertex[cur]
+    path = getPath(start,goal,prevVertex)
     return (path, shortestDistanceFromStart[goal])
+
+    
+    
+    
+print(dijkstra_reddit(graph, 'a', 'h'))
